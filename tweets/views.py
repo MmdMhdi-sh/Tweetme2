67a00b885822f4d8f4352ac9ddd2ebcb492b3d70
mmdmhdi <mmdmhdi.shhr@gmail.com>
2024-08-04 16:21:13 +0330

    Detail REST API

--- tweets/views.py
+++ tweets/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
 
 from .models import Tweet
@@ -7,8 +7,15 @@ def home_view(request, *args, **kwargs):
     return HttpResponse("<h1>Hello World</h1>")
 
 def tweet_detail_view(request, tweet_id,*args, **kwargs):
+    data = {
+        "id": tweet_id,
+        # "image_path": obj.image.url
+    }
+    status = 200
     try:
         obj = Tweet.objects.get(id=tweet_id)
+        data["content"] = obj.content
     except:
-        raise Http404
-    return HttpResponse(f"<h1>{obj.id} - {obj.content}</h1>")
+        data["message"] = "Not Found"
+        status = 404
+    return JsonResponse(data, status=status)
