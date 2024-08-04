42bdf7d2b47d3aa85904cd640422a56e0de145fe
mmdmhdi <mmdmhdi.shhr@gmail.com>
2024-08-04 14:50:02 +0330

    Tweet Detail View

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Tweet
+
+def home_view(request, *args, **kwargs):
+    return HttpResponse("<h1>Hello World</h1>")
+
+def tweet_detail_view(request, tweet_id,*args, **kwargs):
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+    except:
+        raise Http404
+    return HttpResponse(f"<h1>{obj.id} - {obj.content}</h1>")
