0c40c6a206eb2185fa35d4fe934ac67eedc5b7bb
mmdmhdi <mmdmhdi.shhr@gmail.com>
2024-08-07 17:04:44 +0330

    tweets list JS

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,5 @@
+import random
+
 from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
 
@@ -6,7 +8,26 @@ from .models import Tweet
 def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html", context={}, status=200)
 
+def tweet_list_view(request, *args, **kwargs):
+    """
+    REST API View
+    Consume by JS
+    Return json data
+    """
+    qs = Tweet.objects.all()
+    tweets_list = [{"id": x.id, "content": x.content, "likes": random.randint(0, 12312)} for x in qs]
+    data = {
+        "isUser": False,
+        "response": tweets_list
+    }
+    return JsonResponse(data)
+
 def tweet_detail_view(request, tweet_id,*args, **kwargs):
+    """
+    REST API View
+    Consume by JS
+    Return json data
+    """
     data = {
         "id": tweet_id,
         # "image_path": obj.image.url
