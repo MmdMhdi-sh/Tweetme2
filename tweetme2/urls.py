2c7dfd3940bb1d092b89f922afe6010b276de600
mmdmhdi <mmdmhdi.shhr@gmail.com>
2024-08-22 22:08:51 +0330

    profiles app and signal to create profile

--- tweetme2/urls.py
+++ tweetme2/urls.py
@@ -17,13 +17,12 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 
 from tweets.views import (
     tweets_detail_view,
     tweets_list_view,
-    tweets_profile_view,
 )
 from accounts.views import (
     login_view,
@@ -38,7 +37,7 @@ urlpatterns = [
     path('logout/', logout_view),
     path('register/', register_view),
     path('<int:tweet_id>/', tweets_detail_view),
-    path('profile/<str:username>/', tweets_profile_view),
+    re_path(r'profiles?/', include('profiles.urls')),
     path('api/tweets/', include('tweets.api.urls'))
 ]
 
--- tweets/views.py
+++ tweets/views.py
@@ -12,16 +12,11 @@ def home_view(request, *args, **kwargs):
     username = None
     if request.user.is_authenticated:
         username = request.user.username
-    return render(request, "pages/home.html", context={}, status=200)
+    return render(request, "pages/home.html", context={"username": username}, status=200)
 
 def tweets_list_view(request, *args, **kwargs):
-    username = None
-    if request.user.is_authenticated:
-        username = request.user.username
     return render(request, "tweets/list.html")
 
 def tweets_detail_view(request, tweet_id, *args, **kwargs):
     return render(request, "tweets/detail.html", context={"tweet_id": tweet_id})
 
-def tweets_profile_view(request, username, *args, **kwargs):
-    return render(request, "tweets/profile.html", context={"profiel_username": username}, status=200)
